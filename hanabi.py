0ca9e42a7c092f0c743e26087d998f193709c549
ccanela <cristina.canela-anon@insa-lyon.fr>
2024-01-19 11:25:07 +0100

    test pygame

--- hanabi.py
+++ hanabi.py
@@ -1,1 +1,23 @@
-import 
+import pygame 
+import sys 
+import paramiko
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Hanabi Game')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Update the screen
+    pygame.display.flip()
+
+    
+play_pile = []
+discarded_cards = []
+players_cards: {
+    "player1": [], 
+    "player2": []
+    }                   #on pourra rajouter les autres joueurs
