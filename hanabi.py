d166ed2ced1d079ac3ad96a3fb7fee489c400b80
ninarodriguezz <nina.rodriguez@insa-lyon.fr>
2024-01-24 23:56:43 +0100

    séparation game et player

--- hanabi.py
+++ hanabi.py
@@ -50,86 +50,8 @@ class HanabiGame:
                 self.players_cards[f"player{player+1}"].append(card)
                 self.playersCards_sem.release()
 
-    def turn(self, player):
-        print(f"It's {player}'s turn.")
-        
-        if self.info_tk < 0 :
-            #Arreter d'afficher l'option give a hint
-            pass
-        else :
-            #Afficher l'option give a hint    
-            pass
-        
-        action = input("Choose an action (give hint or play card): ")
-        # il manque: check if info_tk > 0 
-        if action == "give hint":
-            # Ensure it's not another player's turn
-            # Implement the logic for giving a hint here
-            self.give_hint(player)
-
-        elif action == "play card":
-            # Implement the logic for playing a card here
-            self.play_card(player)
-        
-        elif action == "discard":
-            self.discard(player)    
-        else:
-            print("Invalid action. Please try again.")
-
-    def give_hint(self, player):
-        #avant d'appeler la fonction il faut voir s'il y a de info_tk disponibles c-a-d info_tk >  
-        
-        self.tokens_sem.acquire()
-        self.info_tk -= 1 
-        self.tokens_sem.release()
-        
-        #Arreter d'afficher les boutons d'option
-        #Afficher indication pour choisir carte puis piece number or color
-        """ 
-        if hint_type == "color":
-            color = card['color']
-            cards_of_color = [card for card in self.players_cards[teammate] if card['color'] == color]
-            self.playersCards_sem.acquire()
-            for card in self.players_cards[teammate]:
-                if card in cards_of_color:
-                    card["hint_color"] = True
-            self.playersCards_sem.release()        
-                        
-                
-        if hint_type == "number":
-            num = card['number']
-            cards_of_number = [card for card in self.players_card[teammate] if card['number'] == number]
-            self.playersCards_sem.acquire()
-            for card in self.players_cards[teammate]:
-                if card in cards_of_number:
-                    card["hint_number"] = True
-            self.playersCards_sem.release()  
-        """   
-             
             
-
-
-
-    """ 
-    def discard_card(self, player):
-        # Avant de faire l'appel on doit regarder que note_tk_used > 0 sinon on peut pas "discard_card"
-        self.note_tk_used -= 1
-        self.note_tk += 1
-
-        #card_to_discard = le jouer choisira a travers l'interface graphique 
-
-        # Remove the chosen card from the player's hand
-        self.players_cards[player].remove(card_to_discard)
-
-        # Add the discarded card to the discard pile
-        self.discarded_cards.append(card_to_discard)
-
-        # Draw another card into the player's hand
-        if len(self.deck) > 0:
-            new_card = self.deck.pop()
-            self.players_cards[player].append(new_card)
-
-    """
+    
     def play_card(self, player):
         #code pour choisir une carte et l'enlever (pop) de players_cards[f"player{player}"](avec interface graphique)
 
@@ -202,24 +124,7 @@ class HanabiGame:
         for player_process in player_processes:
             player_process.join()
 
-    def player_process(self, player_num, player_pipe):
-        player_name = f"player{player_num}"
-        player_hand = self.players_cards[player_name]
-        HOST_int = "localhost"
-        PORT_int = 6666 + player_num
-
-        server_socket_int = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket_int.bind((HOST_int, PORT_int))
-        server_socket_int.listen(1)
-        client_socket_int, address = server_socket_int.accept()
-
-        running = True
-        while running:
-            
-            pass
-            # Lógica del jugador aquí
-            # Manejar la comunicación con el juego a través del pipe y el socket
-
+ 
 if __name__ == "__main__":
     num_players = int(input("Enter the number of players (from 2 to 5): "))
     while num_players < 2 or num_players > 5:
@@ -259,3 +164,26 @@ pygame.quit()
 #game_process.join()
 
  """
+ 
+ 
+ 
+""" 
+def discard_card(self, player):
+    # Avant de faire l'appel on doit regarder que note_tk_used > 0 sinon on peut pas "discard_card"
+    self.note_tk_used -= 1
+    self.note_tk += 1
+
+    #card_to_discard = le jouer choisira a travers l'interface graphique 
+
+    # Remove the chosen card from the player's hand
+    self.players_cards[player].remove(card_to_discard)
+
+    # Add the discarded card to the discard pile
+    self.discarded_cards.append(card_to_discard)
+
+    # Draw another card into the player's hand
+    if len(self.deck) > 0:
+        new_card = self.deck.pop()
+        self.players_cards[player].append(new_card)
+
+"""
